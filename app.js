9a8dbcba2874ba0e26612a1e97be6f16720f39ce
williamdampier <74879032+williamdampier@users.noreply.github.com>
2021-09-15 15:12:09 +1200

    Add files via upload

--- app.js
+++ app.js
@@ -92,5 +92,5 @@ const requestedPostId = req.params.postId;
 
 
 app.listen(process.env.PORT || 3000, function() {
-  console.log("Server started on port 3000");
+  console.log("Server started");
 });
