71066f4da48611e87f4a681c23d5f3e2e9f93c84
williamdampier <74879032+williamdampier@users.noreply.github.com>
2021-09-15 14:20:19 +1200

    Add files via upload

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ var _ = require('lodash');
 
 const mongoose = require('mongoose');
 
-mongoose.connect("mongodb+srv://adminvladi:pswd@cluster0.cctbj.mongodb.net/blogWebsite", { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false }); //connect to DB
+mongoose.connect("mongodb+srv://"+process.env.USER_NAME+":"+process.env.PASSWORD+"@cluster0.ibf12.mongodb.net/EJSBlog", { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false }); //connect to DB
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
